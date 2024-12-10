08266086e31dd5bb86027ab8a6d2439075342c4c
lech.piechota <lech.piechota@ocado.com>
2024-12-10 06:36:25 +0100

    solution for day09 part2 (tmp)

--- solutions/day09/solution_day09.py
+++ solutions/day09/solution_day09.py
@@ -34,3 +34,67 @@ def count_checksum(data: str) -> int:
             disc_index += 1
 
     return checksum
+
+
+def count_checksum_v2(data: str) -> int:
+    disc_elements = []
+    for i in range(len(data)):
+        disc_elements.append(to_disc_element(int(data[i]), i))
+
+    while True:
+        file_id = last_unchecked_file_id(disc_elements)
+        if file_id == -1:
+            break
+        else:
+            size, element, checked, index = disc_elements[file_id]
+            space_found = False
+            for i in range(file_id):
+                space = disc_elements[i]
+                if space[1] == "s" and space[0] >= size:
+                    disc_elements.pop(file_id)
+                    disc_elements.insert(file_id, (size, "s", False, 0))
+                    disc_elements.pop(i)
+                    disc_elements.insert(i, (size, "f", True, index))
+                    if space[0] > size:
+                        disc_elements.insert(i+1, (space[0]-size, "s", False, 0))
+                    space_found = True
+                    break
+            if not space_found:
+                disc_elements[file_id] = (size, element, True, index)
+
+    return calculate_checksum(disc_elements)
+
+
+def calculate_checksum(disc_elements: list[tuple[int, str, bool, int]]) -> int:
+    checksum = 0
+    index_on_disc = 0
+    for i in range(len(disc_elements)):
+        size, element, checked, index = disc_elements[i]
+        for j in range(size):
+            if element == "f":
+                checksum += index_on_disc*index
+            index_on_disc += 1
+    return checksum
+
+def print_disc_elements(disc_elements: list[tuple[int, str, bool, int]]):
+    printout = ""
+    for i in range(len(disc_elements)):
+        size, element, checked, index = disc_elements[i]
+        if element == "f":
+            printout += str(index)*size
+        if element == "s":
+            printout += "."*size
+    print(printout)
+
+def to_disc_element(size: int, index: int) -> tuple[int, str, bool, int]:
+    if index%2==0:
+        return size, "f", False, int(index/2)
+    else:
+        return size, "s", False, 0
+
+def last_unchecked_file_id(disc_elements: list[tuple[int, str, bool, int]]) -> int:
+    for i in range(len(disc_elements)-1, 0, -1):
+        size, element, checked, index = disc_elements[i]
+        if element == "f" and not checked:
+            return i
+    return -1
--- tests/day09/test_solution_day09.py
+++ tests/day09/test_solution_day09.py
@@ -1,15 +1,19 @@
 import unittest
 
 from solutions.common.readers import read_strings, read
-from solutions.day09.solution_day09 import count_checksum
+from solutions.day09.solution_day09 import count_checksum, count_checksum_v2
 
 example = read_strings("test1.txt")
 
 
 class TestDay07Unit(unittest.TestCase):
     def test_count_checksum(self):
-        self.assertEqual(2+4+3+4+5+12+14+16, count_checksum("12345"))
+        self.assertEqual(2 + 4 + 3 + 4 + 5 + 12 + 14 + 16, count_checksum("12345"))
         self.assertEqual(1928, count_checksum("2333133121414131402"))
+
+    def test_count_checksum_v2(self):
+        self.assertEqual(2858, count_checksum_v2("2333133121414131402"))
+
 
 class TestDay07Solution(unittest.TestCase):
     def test_total_calibration_result(self):
@@ -20,13 +24,13 @@ class TestDay07Solution(unittest.TestCase):
         _input = read("input.txt")
         print(count_checksum(_input))
 
-    # def test_total_calibration_result_v2(self):
-    #     input_test_1 = read_strings("test1.txt")
-    #     self.assertEqual(11387, total_calibration_result_v2(input_test_1))
-    #
-    # def test_result_on_real_input_part2(self):
-    #     _input = read_strings("input.txt")
-    #     print(total_calibration_result_v2(_input))
+    def test_total_calibration_result_v2(self):
+        input_test_1 = read("test1.txt")
+        self.assertEqual(2858, count_checksum_v2(input_test_1))
+
+    def test_result_on_real_input_part2(self):
+        _input = read("input.txt")
+        print(count_checksum_v2(_input))
 
 
 if __name__ == '__main__':
