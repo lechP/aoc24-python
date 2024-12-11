f72d63f8663a3db36acb4402b73af19deb4e4335
lech.piechota <lech.piechota@ocado.com>
2024-12-11 11:59:04 +0100

    day11 refactoring

--- solutions/day11/solution_day11.py
+++ solutions/day11/solution_day11.py
@@ -19,7 +19,7 @@ def process_list(nums: list[int]) -> list[int]:
     return nums
 
 
-# suboptimal solution - takes ~3minutes to run
+# suboptimal solution - takes ~3minutes to run for part1
 def solution_day11_naive(data) -> int:
     nums = list(map(int, data.split(" ")))
     loops = 25
@@ -36,7 +36,7 @@ def solution_day11(data) -> int:
         total += process_num_v2(num, 25)
     return total
 
-
+# simple recursive approach - count the length for given initial number
 def process_num_v2(num: int, loops: int) -> int:
     if loops == 0:
         return 1
@@ -50,34 +50,34 @@ def process_num_v2(num: int, loops: int) -> int:
         else:
             return process_num_v2(num * 2024, loops - 1)
 
-def solution(data) -> int:
+
+def solution_day11_part2(data) -> int:
     nums = list(map(int, data.split(" ")))
     total = 0
     for num in nums:
         total += process_num_with_memo(num, 75)
     return total
 
+
 # third approach using memoization
 cache = {}
+
 def process_num_with_memo(num: int, loops: int) -> int:
     key = (num, loops)
     if key in cache:
         return cache[key]
+
+    if loops == 0:
+        res = 1
     else:
-        if loops == 0:
-            return 1
+        if num == 0:
+            res = process_num_with_memo(1, loops - 1)
+        elif len(str(num)) % 2 == 0:
+            s = str(num)
+            half = len(s) // 2
+            res = process_num_with_memo(int(s[:half]), loops - 1) + process_num_with_memo(int(s[half:]), loops - 1)
         else:
-            if num == 0:
-                res = process_num_with_memo(1, loops - 1)
-                cache[key] = res
-                return res
-            elif len(str(num)) % 2 == 0:
-                s = str(num)
-                half = len(s) // 2
-                res = process_num_with_memo(int(s[:half]), loops - 1) + process_num_with_memo(int(s[half:]), loops - 1)
-                cache[key] = res
-                return res
-            else:
-                res = process_num_with_memo(num * 2024, loops - 1)
-                cache[key] = res
-                return res
+            res = process_num_with_memo(num * 2024, loops - 1)
+
+    cache[key] = res
+    return res
