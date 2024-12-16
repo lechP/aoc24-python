11ba60876762f4fb74ec83d46a2eb15fc2e9f1b0
lech.piechota <lech.piechota@ocado.com>
2024-12-16 22:17:38 +0100

    day16 part2: suboptimal solution

--- solutions/day16/solution_day16.py
+++ solutions/day16/solution_day16.py
@@ -1,4 +1,4 @@
-def parse_input(data: str) -> tuple[dict[tuple[int, int], str], tuple[int, int], str, int]:
+def parse_input(data: str) -> tuple[dict[tuple[int, int], str], list[tuple[int, int]], tuple[int, int], str, int]:
     lines = data.split("\n")
     cells = {}
     starting_point = None
@@ -9,7 +9,7 @@ def parse_input(data: str) -> tuple[dict[tuple[int, int], str], tuple[int, int],
                 cells[(i, j)] = lines[i][j]
             elif cell == start:
                 starting_point = (i, j)
-    return cells, starting_point, '>', 0
+    return cells, [starting_point], starting_point, '>', 0
 
 def end_position(data: str) -> tuple[int, int]:
     lines = data.split("\n")
@@ -27,7 +27,14 @@ directions = {
     'v': (1, 0)
 }
 
-def next_states(left_cells: dict[tuple[int, int], str], current: tuple[int, int], direction: str, score: int, tmp_scores: dict[tuple[tuple[int, int], str], int]) -> list[tuple[dict[tuple[int, int], str], tuple[int, int], str, int]]:
+def next_states(
+        left_cells: dict[tuple[int, int], str],
+        visited: list[tuple[int, int]],
+        current: tuple[int, int],
+        direction: str,
+        score: int,
+        tmp_scores: dict[tuple[tuple[int, int], str], int]
+) -> list[tuple[dict[tuple[int, int], str], list[tuple[int, int]], tuple[int, int], str, int]]:
     next_ = []
     for d in directions:
         new_position = (current[0] + directions[d][0], current[1] + directions[d][1])
@@ -40,41 +47,43 @@ def next_states(left_cells: dict[tuple[int, int], str], current: tuple[int, int]
                 new_score = score + 1
 
             if (new_position, d) in tmp_scores:
-                if tmp_scores[(new_position, d)] > new_score: #current path is better
+                if tmp_scores[(new_position, d)] >= new_score: #current path is better
                     tmp_scores[(new_position, d)] = new_score
-                    next_.append((new_cells, new_position, d, new_score))
+                    next_.append((new_cells, visited.copy() + [new_position], new_position, d, new_score))
                 # if path is worse than existing one, abandon it
             else:
                 tmp_scores[(new_position, d)] = new_score
-                next_.append((new_cells, new_position, d, new_score))
+                next_.append((new_cells, visited.copy() + [new_position], new_position, d, new_score))
     return next_
 
 # suboptimal - took ~7min, DFS would be better
-def solution_day16(data) -> int:
+def solution_day16(data, known_min_score = None) -> tuple[int, int]:
     states_to_check = [parse_input(data)]
     end_pos = end_position(data)
-    min_score = None
+    min_score = known_min_score
     tmp_scores = {}
+    visited_cells_on_min_paths = set()
     i=0
     while states_to_check:
         state = states_to_check.pop()
         i+=1
         print(f"Steps left: {len(state[0])}")
-        if min_score is None or state[3] < min_score: # otherwise no need to check
+        if min_score is None or state[4] < min_score: # otherwise no need to check
             next_candidates = next_states(*state, tmp_scores)
             next_states_to_check = []
             for n in next_candidates:
-                if n[1] == end_pos:
+                if n[2] == end_pos:
                     if min_score is None:
-                        min_score = n[3]
+                        min_score = n[4]
                     else:
-                        if n[3] < min_score:
-                            min_score = n[3]
+                        if n[4] <= min_score:
+                            min_score = n[4]
+                            visited_cells_on_min_paths.update(set(n[1]))
                 else:
                     next_states_to_check.append(n)
             states_to_check.extend(next_states_to_check)
     print(f"States checked: {i}")
-    return min_score
+    return min_score, len(visited_cells_on_min_paths)
 
 def solution_day16_part2(data) -> int:
     return 0
--- tests/day16/test_solution_day16.py
+++ tests/day16/test_solution_day16.py
@@ -24,16 +24,16 @@ example = """#################
 
 class TestDay16Unit(unittest.TestCase):
     def test_example(self):
-        self.assertEqual(11048, solution_day16(example))
+        self.assertEqual((11048, 64), solution_day16(example, 11048))
 
 class TestDay16Solution(unittest.TestCase):
     def test_on_test_data_part1(self):
         input_test_1 = read("test1.txt")
-        self.assertEqual(7036, solution_day16(input_test_1))
+        self.assertEqual((7036, 45), solution_day16(input_test_1, 7036))
 
     def test_result_on_real_input_part1(self):
         _input = read("input.txt")
-        print(solution_day16(_input))
+        print(solution_day16(_input, 73404))
 
     def test_on_test_data_part2(self):
         input_test_1 = read("test1.txt")
