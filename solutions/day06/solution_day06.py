e9bee9cdad0ea49dbd3e8f143ef2a0231544f192
lech.piechota <lech.piechota@ocado.com>
2024-12-18 18:49:11 +0100

    day6 part2

--- solutions/day06/solution_day06.py
+++ solutions/day06/solution_day06.py
@@ -1,4 +1,4 @@
-def count_visited(data: list[str]) -> int:
+def solution_day06(data: list[str]) -> int:
     visited = set()
     x, y = initial_position(data)
     _dir = initial_dir
@@ -9,19 +9,19 @@ def count_visited(data: list[str]) -> int:
             visited.add((x, y))
     return len(visited)
 
+
 def move(x: int, y: int, _dir: str, data: list[str]) -> tuple[int, int, str]:
+    while (in_bounds(x + directions[_dir][0], y + directions[_dir][1], data)
+           and data[x + directions[_dir][0]][y + directions[_dir][1]] == obstacle): # do turn as long as there is an obstacle
+        _dir = dir_switches[_dir]
     dx = x + directions[_dir][0]
     dy = y + directions[_dir][1]
-    if not in_bounds(dx, dy, data):
-        return dx, dy, _dir
-    if data[dx][dy] == obstacle:
-        _dir = dir_switches[_dir]
-        dx = x + directions[_dir][0]
-        dy = y + directions[_dir][1]
     return dx, dy, _dir
+
 
 def in_bounds(x: int, y: int, data: list[str]) -> bool:
     return 0 <= x < len(data) and 0 <= y < len(data[0])
+
 
 def initial_position(data: list[str]) -> tuple[int, int]:
     for i in range(len(data)):
@@ -29,6 +29,7 @@ def initial_position(data: list[str]) -> tuple[int, int]:
             if data[i][j] == initial_dir:
                 return i, j
     raise Exception("Initial position not found")
+
 
 directions = {
     ">": (0, 1),
@@ -46,3 +47,40 @@ dir_switches = {
 
 obstacle = '#'
 initial_dir = '^'
+
+
+def solution_day06_part2(data: list[str]) -> int:
+    initial_path = get_initial_path(data)
+    loop_obstacles = set()
+    init_pos = initial_position(data)
+    for step in initial_path:
+        x, y, _ = step
+        if (x, y) != init_pos:
+            data_copy = data.copy()
+            data_copy[x] = data_copy[x][:y] + obstacle + data_copy[x][y + 1:]
+            if falls_into_a_loop(data_copy):
+                loop_obstacles.add((x, y))
+    return len(loop_obstacles)
+
+
+def falls_into_a_loop(data: list[str]) -> bool:
+    visited = set()
+    x, y = initial_position(data)
+    _dir = initial_dir
+    while in_bounds(x, y, data):
+        x, y, _dir = move(x, y, _dir, data)
+        if (x, y, _dir) in visited:
+            return True
+        visited.add((x, y, _dir))
+    return False
+
+
+def get_initial_path(data: list[str]):
+    visited = []
+    x, y = initial_position(data)
+    _dir = initial_dir
+    while in_bounds(x, y, data):
+        x, y, _dir = move(x, y, _dir, data)
+        if in_bounds(x, y, data):
+            visited.append((x, y, _dir))
+    return visited
--- tests/day06/test_solution_day06.py
+++ tests/day06/test_solution_day06.py
@@ -1,7 +1,7 @@
 import unittest
 
 from solutions.common.readers import read, read_strings
-from solutions.day06.solution_day06 import count_visited
+from solutions.day06.solution_day06 import solution_day06, solution_day06_part2
 
 example = read("test1.txt")
 
@@ -11,21 +11,21 @@ class TestDay06Unit(unittest.TestCase):
         pass
 
 class TestDay06Solution(unittest.TestCase):
-    def test_sum_valid_pages(self):
+    def test_part1(self):
         input_test_1 = read_strings("test1.txt")
-        self.assertEqual(41, count_visited(input_test_1))
+        self.assertEqual(41, solution_day06(input_test_1))
 
     def test_result_on_real_input_part1(self):
         _input = read_strings("input.txt")
-        print(count_visited(_input))
+        print(solution_day06(_input))
 
-    # def test_count_mas_cross(self):
-    #     input_test_1 = read("test1.txt")
-    #     self.assertEqual(123, sum_revalidated_pages(input_test_1))
-    #
-    # def test_result_on_real_input_part2(self):
-    #     _input = read("input.txt")
-    #     print(sum_revalidated_pages(_input))
+    def test_part2(self):
+        input_test_1 = read_strings("test1.txt")
+        self.assertEqual(6, solution_day06_part2(input_test_1))
+
+    def test_part2_on_real_input_part2(self):
+        _input = read_strings("input.txt")
+        print(solution_day06_part2(_input))
 
 
 if __name__ == '__main__':
